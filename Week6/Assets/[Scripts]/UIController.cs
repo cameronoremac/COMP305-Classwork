89ad1ab55ecf448f9c285eb435515a2c03ac2061
Cameron Miller <cmille53@my.centennialcollege.ca>
2022-03-02 16:24:59 -0500

    Added Custom Hover Event for StartButton

--- Week6/Assets/[Scripts]/UIController.cs
+++ Week6/Assets/[Scripts]/UIController.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    public CanvasRenderer startButtonRenderer;
+
     public void OnStartButton_Pressed()
     {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void OnStartButton_Over()
+    {
+        startButtonRenderer.SetAlpha(0.5f);
+    }
+
+    public void OnStartButton_Out()
+    {
+        startButtonRenderer.SetAlpha(1.0f);
     }
 }
